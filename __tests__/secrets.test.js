057e7f375a0091e621884606b3c601e1db534aef
Emma Egstad <emma@egstad.com>
2022-03-29 19:47:35 -0500

    updating tests for creating a secret and getting all secrets to require authentication

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -3,6 +3,14 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const Secret = require('../lib/models/Secret');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
 
 describe('backend-top-secret routes', () => {
   beforeEach(() => {
@@ -14,11 +22,16 @@ describe('backend-top-secret routes', () => {
   });
 
   it('should create a secret', async () => {
+    const agent = request.agent(app);
+    await UserService.create({ ...mockUser });
+    const { email, password } = mockUser;
+    await agent.post('/api/v1/users/sessions').send({ email, password });
+
     const expected = {
       title: 'Minnie',
       description: 'is so cute',
     };
-    const res = await request(app).post('/api/v1/secrets').send(expected);
+    const res = await agent.post('/api/v1/secrets').send(expected);
 
     expect(res.body).toEqual({
       id: expect.any(String),
@@ -27,9 +40,14 @@ describe('backend-top-secret routes', () => {
     });
   });
 
-  it('should get all secrets', async () => {
-    const expected = await Secret.findAll();
-    const res = await request(app).get('/api/v1/secrets');
+  it('should get all secrets if a user is signed in', async () => {
+    const agent = request.agent(app);
+    await UserService.create({ ...mockUser });
+    const { email, password } = mockUser;
+    await agent.post('/api/v1/users/sessions').send({ email, password });
+
+    const expected = [];
+    const res = await agent.get('/api/v1/secrets');
 
     expect(res.body).toEqual(expected);
   });
